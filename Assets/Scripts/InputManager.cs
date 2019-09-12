62c317bb1b28e6bf787af4f65baa8b79e63469a1
AndrewCPartridge <46792453+AndrewCPartridge@users.noreply.github.com>
2019-09-12 10:56:09 -0600

    Initial commit

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class InputManager : MonoBehaviour
+{
+    public Text outText;
+    public InputField textIn;
+    public Button enterButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        outText.text = " ";
+    }
+
+    // Update is called once per frame
+    public void EnterPressed()
+    {
+        outText.text = Utilities.ProcessText(textIn.text);
+    }
+
+}
---
+++ Assets/Scripts/Utilities.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utilities
+{
+
+    public static string ProcessText(string input)
+    {
+        // extra cred
+
+        //code goes here
+        //determine whether user has a word or a number
+        //return 'word' if word (includes ab.23cd)
+        //return"number" if number(includes 1.3) etc
+        //float num;
+
+        // if(float.TryParse(input, out num))
+        // {
+        //   input = "number";
+
+        //}
+        // else
+        //{
+        //  input = "word";
+        //  }
+        int words = 1;
+        int letters = 0;
+        decimal average;
+
+
+        foreach (char word in input)
+        {
+            if (char.IsSeparator(word))
+            {
+                words++;
+
+            }
+        }
+            foreach(char letter in input)
+            {
+                if (char.IsLetter(letter) && !char.IsSeparator(letter))
+                {
+                    letters++;
+                }
+                    
+            }
+            average = letters / words;
+            return average.ToString();
+        
+        
+        //return input;
+    }
+}
+
